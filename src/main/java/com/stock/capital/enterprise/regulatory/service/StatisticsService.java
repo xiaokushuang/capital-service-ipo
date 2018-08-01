431b82a419479378c2c8d5130ce1be6701a6c0b0
hezheng <827089003@qq.com>
2018-08-01 12:09:02 +0800

    修改导出文件信息不全bug

--- src/main/java/com/stock/capital/enterprise/regulatory/service/StatisticsService.java
+++ src/main/java/com/stock/capital/enterprise/regulatory/service/StatisticsService.java
@@ -490,15 +490,7 @@ public class StatisticsService extends BaseService {
                 cell.setCellValue(comDtos.get(i).getBelongTrade());
                 cell.setCellStyle(conCenterStyle);
                 cell = row.createCell(3);
-                if("00".equals(comDtos.get(i).getQuasiListedLand())){
-                	cell.setCellValue("上海证券交易所");
-                }else if("01".equals(comDtos.get(i).getQuasiListedLand())){
-                	cell.setCellValue("深圳证券交易所(主板)");
-                }else if("02".equals(comDtos.get(i).getQuasiListedLand())){
-                	cell.setCellValue("深圳证券交易所(中小板)");
-                }else if("03".equals(comDtos.get(i).getQuasiListedLand())){
-                	cell.setCellValue("深圳证券交易所(创业板)");
-                }
+                cell.setCellValue(comDtos.get(i).getQuasiListedLand());
                 cell.setCellStyle(conCenterStyle);
                 cell = row.createCell(4);
                 cell.setCellValue(comDtos.get(i).getRecommendOrganization());
@@ -510,19 +502,7 @@ public class StatisticsService extends BaseService {
                 cell.setCellValue(comDtos.get(i).getLawFirm());
                 cell.setCellStyle(conCenterStyle);
                 cell = row.createCell(7);
-                if("00".equals(comDtos.get(i).getApproveStatus())){
-                	cell.setCellValue("已受理");
-                }else if("01".equals(comDtos.get(i).getApproveStatus())){
-                	cell.setCellValue("已反馈");
-                }else if("02".equals(comDtos.get(i).getApproveStatus())){
-                	cell.setCellValue("预先披露更新");
-                }else if("03".equals(comDtos.get(i).getApproveStatus())){
-                	cell.setCellValue("已通过发审会");
-                }else if("04".equals(comDtos.get(i).getApproveStatus())){
-                	cell.setCellValue("中止审查");
-                }else if("05".equals(comDtos.get(i).getApproveStatus())){
-                	cell.setCellValue("终止审查");
-                }
+                cell.setCellValue(comDtos.get(i).getApproveStatus());
                 cell.setCellStyle(conCenterStyle);
                 cell = row.createCell(8);
                 if("1".equals(comDtos.get(i).getHasedRandomInspection())){
