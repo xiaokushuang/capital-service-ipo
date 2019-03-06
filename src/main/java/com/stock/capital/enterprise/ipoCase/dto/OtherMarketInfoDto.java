8f461bcd780ebcdd93814c4df4b7a6d33ffeeccf
chenzhengxiu <2318262245@qq.com>
2019-03-06 18:02:39 +0800

    ipo 提交公司概览接口部分代码

---
+++ src/main/java/com/stock/capital/enterprise/ipoCase/dto/OtherMarketInfoDto.java
@@ -0,0 +1,91 @@
+package com.stock.capital.enterprise.ipoCase.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+
+public class OtherMarketInfoDto implements Serializable {
+
+    private static final long serialVersionUID = -8905898062160718903L;
+
+    @ApiModelProperty("主键")
+    private String id;
+
+    @ApiModelProperty("案例id")
+    private String bid;
+
+    @ApiModelProperty("资本市场")
+    private String marketType;
+
+    @ApiModelProperty("公司代码")
+    private String companyCode;
+
+    @ApiModelProperty("证券简称")
+    private String companyName;
+
+    @ApiModelProperty("上市日/挂牌日")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date listTime;
+
+    @ApiModelProperty("退市日/摘牌日")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date delistTime;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getMarketType() {
+        return marketType;
+    }
+
+    public void setMarketType(String marketType) {
+        this.marketType = marketType;
+    }
+
+    public String getCompanyCode() {
+        return companyCode;
+    }
+
+    public void setCompanyCode(String companyCode) {
+        this.companyCode = companyCode;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Date getListTime() {
+        return listTime;
+    }
+
+    public void setListTime(Date listTime) {
+        this.listTime = listTime;
+    }
+
+    public Date getDelistTime() {
+        return delistTime;
+    }
+
+    public void setDelistTime(Date delistTime) {
+        this.delistTime = delistTime;
+    }
+}
---
+++ src/main/java/com/stock/capital/enterprise/ipoCase/dto/SupplierCustomerInfoDto.java
@@ -0,0 +1,218 @@
+package com.stock.capital.enterprise.ipoCase.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * SupplierCustomerInfoDto.
+ *
+ * @author user.
+ * @version 0.0.1.
+ * @serial 2019/2/1 : base version.
+ */
+public class SupplierCustomerInfoDto implements Serializable {
+
+    private static final long serialVersionUID = -1294360425031170599L;
+
+    @ApiModelProperty("主键")
+    private String id;
+
+    @ApiModelProperty("案例id")
+    private String bid;
+
+    @ApiModelProperty("公司代码")
+    private String companyCode;
+
+    @ApiModelProperty("证券简称")
+    private String companyName;
+
+    @ApiModelProperty("第一年内容")
+    private String firstYearContent;
+
+    @ApiModelProperty("第一年金额")
+    private BigDecimal firstYearAmount;
+
+    @ApiModelProperty("第一年占比")
+    private BigDecimal firstYearRatio;
+
+    @ApiModelProperty("第二年内容")
+    private String secondYearContent;
+
+    @ApiModelProperty("第二年金额")
+    private BigDecimal secondYearAmount;
+
+    @ApiModelProperty("第二年占比")
+    private BigDecimal secondYearRatio;
+
+    @ApiModelProperty("第三年内容")
+    private String thirdYearContent;
+
+    @ApiModelProperty("第三年金额")
+    private BigDecimal thirdYearAmount;
+
+    @ApiModelProperty("第三年占比")
+    private BigDecimal thirdYearRatio;
+
+    @ApiModelProperty("一期内容")
+    private String onePeriodContent;
+
+    @ApiModelProperty("一期金额")
+    private BigDecimal onePeriodAmount;
+
+    @ApiModelProperty("一期占比")
+    private BigDecimal onePeriodRatio;
+
+    @ApiModelProperty("报告期终止时间")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date reportPeriod;
+
+    @ApiModelProperty("主要类型")
+    private String mainType;//1:供应商 2:客户
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getCompanyCode() {
+        return companyCode;
+    }
+
+    public void setCompanyCode(String companyCode) {
+        this.companyCode = companyCode;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getFirstYearContent() {
+        return firstYearContent;
+    }
+
+    public void setFirstYearContent(String firstYearContent) {
+        this.firstYearContent = firstYearContent;
+    }
+
+    public BigDecimal getFirstYearAmount() {
+        return firstYearAmount;
+    }
+
+    public void setFirstYearAmount(BigDecimal firstYearAmount) {
+        this.firstYearAmount = firstYearAmount;
+    }
+
+    public BigDecimal getFirstYearRatio() {
+        return firstYearRatio;
+    }
+
+    public void setFirstYearRatio(BigDecimal firstYearRatio) {
+        this.firstYearRatio = firstYearRatio;
+    }
+
+    public String getSecondYearContent() {
+        return secondYearContent;
+    }
+
+    public void setSecondYearContent(String secondYearContent) {
+        this.secondYearContent = secondYearContent;
+    }
+
+    public BigDecimal getSecondYearAmount() {
+        return secondYearAmount;
+    }
+
+    public void setSecondYearAmount(BigDecimal secondYearAmount) {
+        this.secondYearAmount = secondYearAmount;
+    }
+
+    public BigDecimal getSecondYearRatio() {
+        return secondYearRatio;
+    }
+
+    public void setSecondYearRatio(BigDecimal secondYearRatio) {
+        this.secondYearRatio = secondYearRatio;
+    }
+
+    public String getThirdYearContent() {
+        return thirdYearContent;
+    }
+
+    public void setThirdYearContent(String thirdYearContent) {
+        this.thirdYearContent = thirdYearContent;
+    }
+
+    public BigDecimal getThirdYearAmount() {
+        return thirdYearAmount;
+    }
+
+    public void setThirdYearAmount(BigDecimal thirdYearAmount) {
+        this.thirdYearAmount = thirdYearAmount;
+    }
+
+    public BigDecimal getThirdYearRatio() {
+        return thirdYearRatio;
+    }
+
+    public void setThirdYearRatio(BigDecimal thirdYearRatio) {
+        this.thirdYearRatio = thirdYearRatio;
+    }
+
+    public String getOnePeriodContent() {
+        return onePeriodContent;
+    }
+
+    public void setOnePeriodContent(String onePeriodContent) {
+        this.onePeriodContent = onePeriodContent;
+    }
+
+    public BigDecimal getOnePeriodAmount() {
+        return onePeriodAmount;
+    }
+
+    public void setOnePeriodAmount(BigDecimal onePeriodAmount) {
+        this.onePeriodAmount = onePeriodAmount;
+    }
+
+    public BigDecimal getOnePeriodRatio() {
+        return onePeriodRatio;
+    }
+
+    public void setOnePeriodRatio(BigDecimal onePeriodRatio) {
+        this.onePeriodRatio = onePeriodRatio;
+    }
+
+    public Date getReportPeriod() {
+        return reportPeriod;
+    }
+
+    public void setReportPeriod(Date reportPeriod) {
+        this.reportPeriod = reportPeriod;
+    }
+
+    public String getMainType() {
+        return mainType;
+    }
+
+    public void setMainType(String mainType) {
+        this.mainType = mainType;
+    }
+}
