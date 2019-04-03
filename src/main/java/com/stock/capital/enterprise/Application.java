978ae10d90846f052079aa1c017a9b9ea6cf2bb6
XueHui <hui.xue@valueonline.cn>
2019-04-03 16:38:05 +0800

    临时取消注册

--- src/main/java/com/stock/capital/enterprise/Application.java
+++ src/main/java/com/stock/capital/enterprise/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.stock")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
