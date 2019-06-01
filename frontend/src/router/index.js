59ef65fa50c1c723a9aa2be687c5ddabc71162c6
liuhuan <huan.liu@valueonline.cn>
2019-06-01 15:33:39 +0800

    公司详情页弹窗修改

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -31,12 +31,12 @@ Vue.use(Router)
 export const constantRouterMap = [
   { path: '/404', component: _import('errorPage/404'), hidden: true },
   { path: '/401', component: _import('errorPage/401'), hidden: true },
-  {
-    path:'/companyDetailPopWin',
-    component: _import('components-demo/companyDetailPopWinComponent'),
-    name:'companyDetailPopWin',
-    meta: {title: '公司详情弹窗', noCache: true}
-  },
+  // {
+  //   path:'/companyDetailPopWin',
+  //   component: _import('components-demo/companyDetailPopWinComponent'),
+  //   name:'companyDetailPopWin',
+  //   meta: {title: '公司详情弹窗', noCache: true}
+  // },
   // {
   //   path: '',
   //   component: Layout,
@@ -95,6 +95,12 @@ export const constantRouterMap = [
         component: _import('financeStatistics/financeSearch'),
         name: 'financeSearch',
         meta: { title: '首页', icon: 'dashboard', noCache: true }
+      },
+      {
+        path:'/companyDetailPopWin',
+        component: _import('components-demo/companyDetailPopWinComponent'),
+        name:'companyDetailPopWin',
+        meta: {title: '公司详情弹窗', noCache: true}
       },
       {
         path: 'securitiesInit',
