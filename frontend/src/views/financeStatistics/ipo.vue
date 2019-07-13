da8305ccb957d317d32093356f336088a2b2836a
lixinwei <xinwei.li@valueonline.cn>
2019-07-13 12:02:44 +0800

    debugger测试

--- frontend/src/views/financeStatistics/ipo.vue
+++ frontend/src/views/financeStatistics/ipo.vue
@@ -69,7 +69,7 @@
                   </span>
                 </span>
               </div>
-            <div style="position: absolute;right: -5%;top: 50%;transform: translate(-50%, -50%);" >
+            <div style="position: absolute;right: -5%;top: 50%;transform: translate(-50%, -50%);" v-if="ipoplatetype" v-show="wxcodeimgload">
               <img :src="wxcodeUrl" style="width: 120px;cursor: pointer;" @click="wxcodeBig" @load="wxcodeLoad">
               <div style="font-size: 12px;color: rgb(255,255,255);text-align: center" v-show="wxcodeimgload">手机扫码可视化查看</div>
             </div>
@@ -431,7 +431,7 @@ export default {
             }
           }
       });
-      this.wxcodeUrl = "/ipo/ipoInterfaceH5/getQrCode?id="+this.caseId2
+      this.wxcodeUrl = "/ipo/ipoInterfaceH5/getQrCode?id="+this.caseId2+"&access_token="+this.$store.state.app.token
     },
     // 展开全部
     expandAll(flag) {
