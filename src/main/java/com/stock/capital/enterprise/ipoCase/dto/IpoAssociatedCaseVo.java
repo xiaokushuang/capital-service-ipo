07a239dd74d432953e87d540bf35a0bc2272e8f1
duxinyu <xinyu.du@valueonline.cn>
2019-07-15 17:02:16 +0800

    新增序号

--- src/main/java/com/stock/capital/enterprise/ipoCase/dto/IpoAssociatedCaseVo.java
+++ src/main/java/com/stock/capital/enterprise/ipoCase/dto/IpoAssociatedCaseVo.java
@@ -4,14 +4,29 @@ public class IpoAssociatedCaseVo {
 
   private static final long serialVersionUID = 1L;
 
+  public String proSort;// 序号
+
   public String caseId;// 案例id
+
   public String progressName;//标题
+
 //  public String orgCode;// 东财内码
+
   public String iecResult;//案例审核结果
+
   public String processTime;//案例日期
+
 //  public String releaseFlag;//是否发布
+
   public String openFlag;// 是否开放
 
+  public String getProSort() {
+    return proSort;
+  }
+
+  public void setProSort(String proSort) {
+    this.proSort = proSort;
+  }
 
   public String getCaseId() {
     return caseId;
