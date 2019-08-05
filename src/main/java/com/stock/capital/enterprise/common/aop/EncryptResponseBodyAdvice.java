24d9a3bfa8587a52d1b89f74043e9a8830552066
yangjian <jian.yang@valueonline.cn>
2019-08-05 14:03:16 +0800

    新加的类

---
+++ src/main/java/com/stock/capital/enterprise/common/aop/EncryptResponseBodyAdvice.java
@@ -0,0 +1,65 @@
+package com.stock.capital.enterprise.common.aop;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.Sets;
+import com.stock.capital.enterprise.ipoInterfaceH5.controller.IpoInterfaceController;
+import com.stock.core.util.CryptoUtil;
+import com.stock.core.util.JsonUtil;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractMappingJacksonResponseBodyAdvice;
+
+import java.lang.reflect.Method;
+import java.util.Set;
+
+@ControllerAdvice
+public class EncryptResponseBodyAdvice extends AbstractMappingJacksonResponseBodyAdvice {
+
+    private static final String ENCRYPT_KEY = "hNkYmsBUvrTd3C3o";
+    private static final String IV = "mLZT7OIx1qOHZaPX";
+    private static final int MOD = 13;
+    private static final Set<String> WHITE_LIST = Sets.newHashSet();
+
+    static {
+        WHITE_LIST.add("/health");
+    }
+
+
+    @Override
+    protected void beforeBodyWriteInternal(MappingJacksonValue bodyContainer, MediaType contentType,
+                                           MethodParameter returnType, ServerHttpRequest request, ServerHttpResponse response) {
+        Method method = returnType.getMethod();
+        Class<?> declaringClass = method.getDeclaringClass();
+//        非这个Controller都加密
+        if (!declaringClass.equals(IpoInterfaceController.class)) {
+            if (canEncrypt(request)) {
+                String body = JsonUtil.toJson(bodyContainer.getValue());
+                String iv = Base64Utils.encodeToString(IV.getBytes(Charsets.UTF_8));
+                String encryptKey = Base64Utils.encodeToString(ENCRYPT_KEY.getBytes(Charsets.UTF_8));
+                String encryptData = CryptoUtil.encrypt(body, encryptKey, CryptoUtil.AES_CIPHER_ALGORITHM_CBC, iv);
+                int start = (encryptData.length() + 32) % MOD;
+                encryptData = encryptData.substring(0, start + 1) + ENCRYPT_KEY + IV + encryptData.substring(start + 1);
+                bodyContainer.setValue(encryptData);
+            }
+        }
+    }
+
+    private boolean canEncrypt(ServerHttpRequest request) {
+        for (String uri : WHITE_LIST) {
+            AntPathRequestMatcher matcher = new AntPathRequestMatcher(uri);
+            ServletServerHttpRequest httpRequest = (ServletServerHttpRequest) request;
+            if (matcher.matches(httpRequest.getServletRequest())) {
+                return Boolean.FALSE;
+            }
+        }
+        return Boolean.TRUE;
+    }
+
+}
