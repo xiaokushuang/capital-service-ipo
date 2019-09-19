917d18a40b9cc6d496fa6a1219829ef09e2cf4c9
lixinwei <xinwei.li@valueonline.cn>
2019-09-19 14:27:45 +0800

    ipo问询回复页面增加加载框

--- frontend/src/views/navMenu/feedback/feedback.vue
+++ frontend/src/views/navMenu/feedback/feedback.vue
@@ -2460,6 +2460,7 @@ export default {
         onlyResponse: onlyResponse
       };
       getSelectQuestionList(param).then(res => {
+        this.flagLoading = true;
         // 当只有一个tab页时
         if (this.tabList.length == 1) {
           if (res.data.result.length > 0) {
