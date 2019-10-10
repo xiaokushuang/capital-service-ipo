8cc9a388814077e68facd2bc5c5e485995b170f3
xiaokushuang <mingshuang.yan@valueonline.cn>
2019-10-10 11:09:09 +0800

    默认展示ipo

--- frontend/src/views/tutoringCase/tutoringCaseList/ipoCaseList.vue
+++ frontend/src/views/tutoringCase/tutoringCaseList/ipoCaseList.vue
@@ -606,7 +606,7 @@
           id:0,
           name:'全部'
         },
-        caseType: "all", // all ipo ipofd  案例类型 三种类型
+        caseType: "ipo", // all ipo ipofd  案例类型 三种类型
         intermediaryName: '',
         issueLawId: '', //上市条件法规id
         tenantInfo: '', //日志
