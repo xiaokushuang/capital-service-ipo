daff63e302197cc67502bfd8f63e7cb8d0b8afff
xiaokushuang <mingshuang.yan@valueonline.cn>
2019-10-12 16:52:29 +0800

    辅导案例

--- frontend/src/views/tutoringCase/processTree/relatedCase.vue
+++ frontend/src/views/tutoringCase/processTree/relatedCase.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="processTree">
-        <div v-if="proList.length>0">
+        <div v-if="proList.proList.length>0">
             <!-- 第一个进程 -->
             <div>
                 <el-row style="padding-left:12px">
                     <el-col :span="24" style="border-left:1px solid #E9E9E9; margin-bottom: -10px;margin-top:10px;padding-bottom: 10px;">
                         <div>
-                             <div v-for="(item,index) in proList">
+                             <div v-for="(item,index) in proList.proList">
                                 <div class="right" >
                                     <div class="border-box">
                                         <span>
-                                            <!-- <span :id="'sign' +  item.progressIndex" :class="{'circle':item.dateCompare=='1','grayCircle':item.dateCompare=='0'}" v-if="proList.length"> -->
-                                                <span v-if="proList.length" class="circle">
+                                            <!-- <span :id="'sign' +  item.progressIndex" :class="{'circle':item.dateCompare=='1','grayCircle':item.dateCompare=='0'}" v-if="proList.proList.length"> -->
+                                                <span v-if="proList.proList.length" class="circle">
                                                 <!-- 展示序号 【默认时候不加类】-->
                                                 <!-- <span :id="'num' +  item.progressIndex"> -->
                                                 <span>
-                                                    {{proList.length - index}}
+                                                    {{proList.proList.length - index}}
                                                 </span>
                                             </span>
                                         </span>
@@ -23,7 +23,7 @@
                                     <div class="border-right">
                                         <div style="">
                                              <div :style="{'font-size': '14px', 'color': '#14bcf5','display':'inline-block','margin-right':item.caseTitle.length>=14?'-4px':'12px'}"  class="tinyHand">
-                                                <a @click="gonggaoClick(item.caseid,item.openFlag,item.signStatus)">{{item.caseTitle}}</a>
+                                                <a @click="gonggaoClick(item.caseid,item.openFlag)">{{item.caseTitle}}</a>
                                              </div>
                                               <!--<div :style="{'font-size': '14px', 'color': '#14bcf5','display':'inline-block','margin-right':item.caseTitle.slice(0,13).length>=14?'-4px':'12px'}"  class="tinyHand">-->
                                                 <!--<a @click="gonggaoClick(item.caseid,item.openFlag)">{{item.caseTitle.slice(0,13)}}</a>-->
@@ -101,7 +101,8 @@ export default {
     created(){
      },
     mounted() {
-        console.log('关联案例',this.proList)
+        console.log('关联案例',this.proList.proList)
+        console.log('开放公司',this.proList.signStatus)
     },
     components: {
     },
@@ -109,8 +110,8 @@ export default {
 
     },
     methods: {
-       gonggaoClick(id,flag,signStatus) {
-            if (signStatus==="1") {
+       gonggaoClick(id,flag) {
+            if (this.proList.signStatus==="1") {
                 const _self = this;
                 const {href} = _self.$router.resolve({
                 name: 'caseDetail',
