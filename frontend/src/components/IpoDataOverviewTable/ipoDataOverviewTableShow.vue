ffd5f66c893aff80fa9eb90144224211646c0d0e
lixinwei <xinwei.li@valueonline.cn>
2019-10-22 17:55:43 +0800

    修改样式

--- frontend/src/components/IpoDataOverviewTable/ipoDataOverviewTableShow.vue
+++ frontend/src/components/IpoDataOverviewTable/ipoDataOverviewTableShow.vue
@@ -182,7 +182,7 @@ export default {
   border-right:1px solid #14bcf5!important;
 }
 .favorite-table {
-    padding:0 15px 0 10px;
+    padding:0 0 0 10px;
 }
 
 .el-table--border td, .el-table--border th {
