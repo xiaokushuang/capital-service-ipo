d2f0458990c4268a04ad07c52e030e883146f0df
lixinwei <xinwei.li@valueonline.cn>
2019-11-14 14:05:27 +0800

    修改在搜狗浏览器高速模式下导出和下载失败的问题

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -64,7 +64,7 @@ service.interceptors.response.use(
         let link = document.createElement('a')
         link.style.display = 'none';
         link.href = url;
-        link.setAttribute('download', fileName);
+        link.setAttribute('download', fileName)
         link.setAttribute('target', '_top');
         document.body.appendChild(link)
         link.click();
