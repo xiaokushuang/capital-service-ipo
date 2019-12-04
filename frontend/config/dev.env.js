ddc690ba96ee55f668c061140a45e92c058ab89a
lixinwei <xinwei.li@valueonline.cn>
2019-12-04 15:18:23 +0800

    静态分离修改

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	NODE_ENV: '"development"',
 	ENV_CONFIG: '"dev"',
-	BASE_API: '"http://localhost:8080/htrt"',
+	BASE_API: '""',
   CDN_DOMAIN: '"https://static-dev.valueonline.cn"'
 }
