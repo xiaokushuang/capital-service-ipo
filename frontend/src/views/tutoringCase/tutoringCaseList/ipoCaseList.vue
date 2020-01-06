728d79cf177aef3a424e2ef3bde625491c14f73e
lixinwei <xinwei.li@valueonline.cn>
2020-01-06 14:10:42 +0800

    修改检索条件未能清空的bug ，但是在ie上还会出现先消失在显示问题，稍后修改

--- frontend/src/views/tutoringCase/tutoringCaseList/ipoCaseList.vue
+++ frontend/src/views/tutoringCase/tutoringCaseList/ipoCaseList.vue
@@ -198,7 +198,7 @@
               </el-select>
             </el-col>
            <el-col :span='8' class="stockIncreasePan-class">
-             <el-multiple-selection :range="true" :tree-data="optionIssueFee" placeholder="发行费用" size="small full"
+             <el-multiple-selection v-if="issueFeeShow" :range="true" :tree-data="optionIssueFee" placeholder="发行费用" size="small full"
                                     :multiple="false" @keydown.enter.native="querySearch" unit="万元" :ran="optionDto" @sure-click="rangeCallIssueFee"
                                     :left-decimal="true">
              </el-multiple-selection>
@@ -211,7 +211,7 @@
              </el-date-picker>
            </el-col>
             <el-col :span='4' class="stockIncreasePan-class">
-              <el-multiple-selection  :range="true" :tree-data="optionPeIssueA" placeholder="发行后市盈率" size="small full"
+              <el-multiple-selection v-if="issueShow" :range="true" :tree-data="optionPeIssueA" placeholder="发行后市盈率" size="small full"
                                      :multiple="false" @keydown.enter.native="querySearch" unit="倍" :ran="optionDto" @sure-click="rangeCallPeIssueA"
                                      :left-decimal="true">
               </el-multiple-selection>
@@ -227,7 +227,7 @@
               </el-select>
             </el-col>
             <el-col :span='8'>
-              <el-multiple-selection  :range="true" :tree-data="optionAuditDuration" placeholder="审核历时（辅导历时）"
+              <el-multiple-selection v-if="durationShow" :range="true" :tree-data="optionAuditDuration" placeholder="审核历时（辅导历时）"
                                      size="small full" :multiple="false" @keydown.enter.native="querySearch" unit="天" :ran="optionDto" @sure-click="rangeAuditDuration"
                                      :left-decimal="true">
               </el-multiple-selection>
@@ -1125,7 +1125,6 @@
         belongsBureau: '',
         issueShow: true,
         issueFeeShow: true,
-        prospectusShow: true, //招股书最近一次估值
         durationShow: true,
         profitOneShow: true,
         profitTwoShow: true,
@@ -1581,7 +1580,6 @@
         }
         _self.issueShow = false;
         _self.issueFeeShow = false;
-        _self.prospectusShow = false; //招股书最近一次估值
         _self.durationShow = false;
         _self.profitOneShow = false;
         _self.profitTwoShow = false;
@@ -1600,7 +1598,6 @@
         _self.$nextTick(() => {
           _self.issueShow = true;
           _self.issueFeeShow = true;
-          _self.prospectusShow = true; //招股书最近一次估值
           _self.durationShow = true;
           _self.profitOneShow = true;
           _self.profitTwoShow = true;
