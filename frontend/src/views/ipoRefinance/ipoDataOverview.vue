7da09fef4361a2b6f5a10f162a0853af30e051c9
lixinwei <xinwei.li@valueonline.cn>
2020-01-10 17:05:36 +0800

    修改显示位置

--- frontend/src/views/ipoRefinance/ipoDataOverview.vue
+++ frontend/src/views/ipoRefinance/ipoDataOverview.vue
@@ -311,7 +311,7 @@ export default {
       if(lable=="注册生效"){
         lable = "<span style='color:#0099cc'>注册生效</span>";
       }
-      if(lable=="已提交发审会讨论，暂缓表决"){
+      if(lable=="已提交发审会讨论，暂缓表决(已提交上市委会议，暂缓审议)"){
         lable = "已提交发审会讨论，暂缓表决<span style='color:#0099cc'>(已提交上市委会议，暂缓审议)</span>";
       }
       return lable;
