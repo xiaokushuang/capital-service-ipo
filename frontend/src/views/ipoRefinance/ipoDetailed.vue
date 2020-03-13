b5cd7df325d68a3bad2d2cce8bc983355a879a00
duxinyu <xinyu.du@valueonline.cn>
2020-03-13 10:14:29 +0800

    样式样式

--- frontend/src/views/ipoRefinance/ipoDetailed.vue
+++ frontend/src/views/ipoRefinance/ipoDetailed.vue
@@ -30,7 +30,7 @@
     <!-- table -->
     <el-row>
       <el-col :span="24">
-        <el-table :data="data" style="width: 100%" :height="tableHeight" class="paddingControl" border tooltip-effect="dark" ref="multipleSelection"
+        <el-table :data="data" style="width: 100%" :height="600" class="paddingControl" border tooltip-effect="dark" ref="multipleSelection"
           :row-class-name="tableRowClassName" :header-cell-class-name="tableHeaderColor" @sort-change="sortChange">
           <el-table-column label="序号" align="center" :index="indexMethod" type="index" width="60" style="border-right:1px solid #fff"></el-table-column>
           <el-table-column label="注册地" align="center">
@@ -187,11 +187,9 @@
     },
     mounted() {
       debugger;
-      this.$nextTick(() => {
-        console.log("我是屏幕高:" + window.screen.height);
-        this.tableHeight = window.screen.height - 320 - 75;
-        console.log("我是table 高 :L  " + this.tableHeight);
-      })
+      console.log("我是屏幕高:" + window.screen.height);
+      this.tableHeight = window.screen.height - 320 - 75;
+      console.log("我是table 高 :L  " + this.tableHeight);
       // 获取fromFlag 判断是否是独立项目
       var fromFlag = this.$route.query['fromFlag'];
       // 判断是否是独立收费模块
