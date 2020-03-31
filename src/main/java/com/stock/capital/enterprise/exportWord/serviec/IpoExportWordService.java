10c37092736f14b0dbc4af4a096ba6fc7391dec2
duxinyu <xinyu.du@valueonline.cn>
2020-03-31 11:22:42 +0800

    ipo导出word

--- src/main/java/com/stock/capital/enterprise/exportWord/serviec/IpoExportWordService.java
+++ src/main/java/com/stock/capital/enterprise/exportWord/serviec/IpoExportWordService.java
@@ -2,17 +2,24 @@ package com.stock.capital.enterprise.exportWord.serviec;
 
 import com.stock.capital.enterprise.ipoCase.dto.CompanyOverviewVo;
 import com.stock.capital.enterprise.ipoCase.dto.IndustryCompareRateDto;
+import com.stock.capital.enterprise.ipoCase.dto.IpoFinanceDto;
+import com.stock.capital.enterprise.ipoCase.dto.IpoInvestItemDto;
 import com.stock.capital.enterprise.ipoCase.dto.IpoSplitDto;
 import com.stock.capital.enterprise.ipoCase.dto.IpoTechnologyVo;
 import com.stock.capital.enterprise.ipoCase.dto.IpoValuationDto;
+import com.stock.capital.enterprise.ipoCase.dto.IssueDataDto;
+import com.stock.capital.enterprise.ipoCase.dto.IssueFeeDto;
 import com.stock.capital.enterprise.ipoCase.dto.IssuerIndustryStatusDto;
 import com.stock.capital.enterprise.ipoCase.dto.MainCompetitorInfoDto;
 import com.stock.capital.enterprise.ipoCase.dto.MainIncomeVo;
 import com.stock.capital.enterprise.ipoCase.dto.OtherMarketInfoDto;
+import com.stock.capital.enterprise.ipoCase.dto.StrategicPlacementMainDto;
 import com.stock.capital.enterprise.ipoCase.dto.SupplierCustomerMainDto;
 import com.stock.capital.enterprise.ipoCase.service.CompanyOverviewService;
+import com.stock.capital.enterprise.ipoCase.service.IpoFeedbackService;
+import com.stock.capital.enterprise.ipoCase.service.IpoFinanceService;
+import com.stock.capital.enterprise.ipoCase.service.IpoInvestService;
 import com.stock.capital.enterprise.ipoCase.service.IssueSituationService;
-import com.stock.core.dto.JsonResponse;
 import com.stock.core.service.BaseService;
 import java.util.HashMap;
 import java.util.List;
@@ -31,6 +38,15 @@ public class IpoExportWordService extends BaseService {
   @Autowired
   public IssueSituationService issueSituationService;
 
+  @Autowired
+  public IpoFinanceService ipoFinanceService;
+
+  @Autowired
+  public IpoInvestService ipoInvestService;
+
+  @Autowired
+  public IpoFeedbackService ipoFeedbackService;
+
   private static final Logger logger = LoggerFactory.getLogger(IpoExportWordService.class);
 
 
@@ -42,16 +58,18 @@ public class IpoExportWordService extends BaseService {
 
     Map<String,Object> resultMap = new HashMap<>();
 //    二、公司概况
-    CompanyOverviewVo companyInformation = companyOverviewService.getIpoCaseDetail(caseId);//公司概览-拟上市板块 模块
+    //公司概览-拟上市板块 模块
+    //股权结构图， 股权结构标签 structureLabel 图 structureUrl
+    CompanyOverviewVo companyInformation = companyOverviewService.getIpoCaseDetail(caseId);
     List<OtherMarketInfoDto> otherMarkInfo = companyOverviewService.getMarketData(caseId);//公司概览-登陆其他资本市场 模块
     List<IpoSplitDto> splitList = companyOverviewService.getSpliteData(caseId);// 拆分上市情况
     List<IpoValuationDto> valuationList = companyOverviewService.getVluationData(caseId);//最近一次估值情况
-    CompanyOverviewVo equityStructureVo = companyOverviewService.getIpoCaseDetail(caseId);//股权结构图， 股权结构标签 structureLabel 图 structureUrl
+//    CompanyOverviewVo equityStructureVo = companyOverviewService.getIpoCaseDetail(caseId);
     resultMap.put("companyInformation",companyInformation);
     resultMap.put("otherMarkInfo",otherMarkInfo);
     resultMap.put("splitList",splitList);
     resultMap.put("valuationList",valuationList);
-    resultMap.put("equityStructure",equityStructureVo);
+//    resultMap.put("equityStructure",equityStructureVo);
 
 //    三、公司生产经营情况
     MainIncomeVo mainIncomeVo = companyOverviewService.getIncomeData(caseId);//主营业务收入构成
@@ -77,9 +95,38 @@ public class IpoExportWordService extends BaseService {
     resultMap.put("ipoTechnology",ipoTechnologyVo);
 
 //    五、财务信息
+//   财务总体情况：表头日期：dateList 数据（倒序排列）：IpoFinanceOverList
+    IpoFinanceDto ipoFinance = ipoFinanceService.selectFinanceOverList(caseId);
+//   资产与负债情况
+//    dateList 资产类项目列表：ipoAssetItemList 负债类项目列表：ipoDebtItemList 权益类项目列表：ipoEquityItemList
+    IpoFinanceDto assetLiability = ipoFinanceService.selectFinanceList(caseId);
+//    收入与利润情况 表头日期：dateLis 收入类项目：IpoProfitItemList 成本类项目：IpoCostItemList 利润类项目：IpoReturnOverList
+    IpoFinanceDto incomeProfit = ipoFinanceService.selectFinanceProfitList(caseId);
+//    主要财务指标 表头日期：dateLis  主要财务指标：IpoMainIndexList
+    IpoFinanceDto financialIndex = ipoFinanceService.selectMainIndexList(caseId);
+    resultMap.put("ipoFinance",ipoFinance);
+    resultMap.put("assetLiability",assetLiability);
+    resultMap.put("incomeProfit",incomeProfit);
+    resultMap.put("financialIndex",financialIndex);
 
+    //    六、募集资金运用
+    List<IpoInvestItemDto> ipoInvestItem = ipoInvestService.selectInvestItem(caseId);
+    resultMap.put("ipoInvestItem",ipoInvestItem);
+
+//    七、问询与回复（科创板注册制）
+//    List<IpoFeedbackDto> ipoFeedbackList = ipoFeedbackService.selectNewFeedbackList(caseId);//问题数量 回复情况
+
+
+//    十一、发行概况
+    IssueDataDto issueData = issueSituationService.getIssueData(caseId);//发行数据
+    List<IssueFeeDto> issueFeeList = issueSituationService.getIssueFeeData(caseId);//发行费用
+    StrategicPlacementMainDto strategicPlacementMain = issueSituationService.getPlacementData(caseId);//发行费用
+    resultMap.put("issueData",issueData);
+    resultMap.put("issueFeeList",issueFeeList);
+    resultMap.put("strategicPlacementMain",strategicPlacementMain);
 
     end = System.currentTimeMillis();
+
     logger.info("导出word api结束" +  "Run Time:" + (end - start) + "(ms)");
     return resultMap;
   }
