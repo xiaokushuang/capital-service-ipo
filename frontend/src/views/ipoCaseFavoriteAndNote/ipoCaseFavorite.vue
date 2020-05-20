648a6b4081bebb82f8ff64481ac48665154cf5f0
lixinwei <xinwei.li@valueonline.cn>
2020-05-20 17:43:34 +0800

    删除alert调试

--- frontend/src/views/ipoCaseFavoriteAndNote/ipoCaseFavorite.vue
+++ frontend/src/views/ipoCaseFavoriteAndNote/ipoCaseFavorite.vue
@@ -694,7 +694,7 @@
           orderByOrder: _self.orderByOrder
         };
         _queryIpoFavoriteList(_data).then(response => {
-          // _self.tableLoading = false;
+           _self.tableLoading = false;
           if (response.data && response.data.success && response.data.result) {
             _self.issueLawId = response.data.result.issueLawId;
             _self.totalCount = response.data.result.total;
