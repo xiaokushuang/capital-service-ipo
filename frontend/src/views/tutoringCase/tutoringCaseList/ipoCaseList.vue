7a169801884b7fba280d1b467dea7f2c2a849f9c
lixinwei <xinwei.li@valueonline.cn>
2020-05-20 11:26:49 +0800

    alert调试

--- frontend/src/views/tutoringCase/tutoringCaseList/ipoCaseList.vue
+++ frontend/src/views/tutoringCase/tutoringCaseList/ipoCaseList.vue
@@ -1942,6 +1942,8 @@
       },
       openNewRuleJxc() {
         const _self = this;
+        debugger;
+        alert(_self.$store.state.app.info);
         const href = window.location.origin + '/ui/laws/laws/lawsDetail?lawId=' + _self.issueLawJxcId + '&access_token=' +
           _self.$store.state.app.token + '&tenant_info=' + _self.$store.state.app.info;
         window.open(href, '_blank');
