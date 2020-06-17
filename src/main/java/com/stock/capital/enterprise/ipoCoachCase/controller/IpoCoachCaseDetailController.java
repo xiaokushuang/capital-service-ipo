03d3e7c2458ad390a37c98ce71d87501421085b1
lixinwei <xinwei.li@valueonline.cn>
2020-06-17 16:54:49 +0800

    修改辅导库计算历时

--- src/main/java/com/stock/capital/enterprise/ipoCoachCase/controller/IpoCoachCaseDetailController.java
+++ src/main/java/com/stock/capital/enterprise/ipoCoachCase/controller/IpoCoachCaseDetailController.java
@@ -12,6 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+
+import static com.stock.core.util.DateUtil.YYYY_MM_DD;
+import static com.stock.core.util.DateUtil.getDate;
+
 @Controller
 @RequestMapping("ipoCoachCaseDetail")
 public class IpoCoachCaseDetailController extends BaseController {
@@ -34,9 +39,16 @@ public class IpoCoachCaseDetailController extends BaseController {
             String listSize = String.valueOf(processResult.getTreeList().get(0).getProList().size() - 1);
             String startDate = "";
             for (int i =0;i< processResult.getTreeList().get(0).getProList().size();i++){
-                if("01".equals(processResult.getTreeList().get(0).getProList().get(i).getProgressType())){
-                    startDate = processResult.getTreeList().get(0).getProList().get(i).getRelaList().get(0).getPublishTime();
-                    break;
+                if("01".equals(processResult.getTreeList().get(0).getProList().get(i).getProgressType()) || "56".equals(processResult.getTreeList().get(0).getProList().get(i).getProgressType())){
+                    if (StringUtils.isNotEmpty(startDate)){
+                        Date dt1 = getDate(startDate, YYYY_MM_DD);
+                        Date dt2 = getDate(processResult.getTreeList().get(0).getProList().get(i).getRelaList().get(0).getPublishTime(), YYYY_MM_DD);
+                        if (dt2.compareTo(dt1)>0){
+                            startDate = processResult.getTreeList().get(0).getProList().get(i).getRelaList().get(0).getPublishTime();
+                        }
+                    }else {
+                        startDate = processResult.getTreeList().get(0).getProList().get(i).getRelaList().get(0).getPublishTime();
+                    }
                 }
             }
             String endDate = processResult.getTreeList().get(0).getProList().get(0).getRelaList().get(0).getPublishTime();
