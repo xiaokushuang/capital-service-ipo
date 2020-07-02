40a8b673dbf7087094111e521fa8b15200574ac0
chenji <ji.chen@valueonline.cn>
2020-07-02 18:56:37 +0800

    添加预发环境前端

---
+++ frontend/config/sim.env.js
@@ -0,0 +1,9 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  ENV_CONFIG: '"sim"',
+  APP_BASE: '"//services-sim.valueonline.cn"',
+  APP_CONTEXT: '"/ui/ipo/"',
+  API_BASE: '"//services-sim.valueonline.cn"',
+  API_CONTEXT: '"/ipo/"',
+  CDN_BASE: '"//static-sim.valueonline.cn"'
+}
